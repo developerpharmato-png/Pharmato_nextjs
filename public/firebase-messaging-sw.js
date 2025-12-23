17f719e63c34bce48854596364504935817ea02c
work technotoil <work.technotoil@gmail.com>
2025-12-23 11:48:57 +0530

    device

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,29 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+"use client";
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js"
+);
 
 firebase.initializeApp({
-  apiKey: "AIzaSyARk63eYnht9p5VD4cad-_S_4VeILqpcqM",
-  authDomain: "pharmato-842d3.firebaseapp.com",
-  projectId: "pharmato-842d3",
-  storageBucket: "pharmato-842d3.firebasestorage.app",
-  messagingSenderId: "1039306398763",
-  appId: "1:1039306398763:web:f4c5cd8118da8cf8a95e6e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 });
 
 const messaging = firebase.messaging();
 
 // Optional: background notification handler
 messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Received background message", payload);
+  console.log(
+    "[firebase-messaging-sw.js] Received background message",
+    payload
+  );
 
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
