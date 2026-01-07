3f0fe10dfb8483f81288ceeb67f3ffa5287d0eaf
work.technotoil <work.technotoil@gmail.com>
2026-01-07 16:08:54 +0530

    order 5

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -7,11 +7,5 @@ export async function POST(req: NextRequest) {
 
   console.log("🔥 CRON CHAL GAYA");
 
-    //   // Example: run every minute
-    // cron.schedule('* * * * *', () => {
-    //   console.log('Scheduler running at', new Date().toISOString());
-    //   // 🔥 Yahan apna task likh sakte ho, jaise DB update ya API call
-    // });
-
   return NextResponse.json({ success: true }); 
 }
