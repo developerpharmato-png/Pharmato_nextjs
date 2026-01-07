fd05ec38549300d547ea1001bdf4f6b7d2aecf9b
work.technotoil <work.technotoil@gmail.com>
2026-01-07 13:14:50 +0530

    order 2

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,10 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from "@/lib/mongodb";
+import cron from 'node-cron';
 
-export async function GET() { 
+export async function POST(req: NextRequest) {
   await dbConnect();
 
   console.log("🔥 CRON CHAL GAYA");
+
+    //   // Example: run every minute
+    // cron.schedule('* * * * *', () => {
+    //   console.log('Scheduler running at', new Date().toISOString());
+    //   // 🔥 Yahan apna task likh sakte ho, jaise DB update ya API call
+    // });
 
   return NextResponse.json({ success: true }); 
 }
