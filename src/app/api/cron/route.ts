c8de6285c18584de654d6e6d9cfbe2d2374740f6
work.technotoil <work.technotoil@gmail.com>
2026-01-07 12:14:09 +0530

    order 9

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/mongodb";
 
-export async function GET() {
+export async function GET() { 
   await dbConnect();
 
   console.log("🔥 CRON CHAL GAYA");
