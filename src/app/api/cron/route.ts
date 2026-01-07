c0d9119052786bde8fe2c9c1fdfb2aa986ec776f
work.technotoil <work.technotoil@gmail.com>
2026-01-07 17:20:00 +0530

    order 7

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from "@/lib/mongodb";
+import cron from 'node-cron';
 import { sendPushNotificationWithData } from '@/utils/firebase.helper';
 
 export async function POST(req: NextRequest) {
@@ -7,11 +8,18 @@ export async function POST(req: NextRequest) {
 
   console.log("🔥 CRON CHAL GAYA");
 
-  await sendPushNotificationWithData({
-    token: `f0dMsw4nTtyZ8ZbRCYY3FC:APA91bHnKR2YYcDnkZPyNC9oVO0Xpi57dCIhWR2DDjYEdmgpBgk2weQ6vp1yjvXiFpqQV4jg7B7pHmRsENhP5mwkUZvFn6eFpcK4b2ZcZ6NpD0SA4M7K-kk`,
-    title: 'Pharmato',
-    body: `🔥 CRON CHAL GAYA`,
-    data: {}
+  // Example: run every minute
+  cron.schedule('* * * * *', async () => {
+
+    console.log("🔥 CRON CHAL GAYA", new Date().toISOString());
+
+    await sendPushNotificationWithData({
+      token: `f0dMsw4nTtyZ8ZbRCYY3FC:APA91bHnKR2YYcDnkZPyNC9oVO0Xpi57dCIhWR2DDjYEdmgpBgk2weQ6vp1yjvXiFpqQV4jg7B7pHmRsENhP5mwkUZvFn6eFpcK4b2ZcZ6NpD0SA4M7K-kk`,
+      title: 'Pharmato',
+      body: `🔥 CRON CHAL GAYA ${new Date().toISOString()}`,
+      data: {}
+    });
+    
   });
 
   return NextResponse.json({ success: true });
