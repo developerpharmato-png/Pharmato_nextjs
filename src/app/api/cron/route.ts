d8db7a21eec91437651a0933f243bbe55175690b
work.technotoil <work.technotoil@gmail.com>
2026-01-07 16:36:41 +0530

    order 6

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,11 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from "@/lib/mongodb";
-import cron from 'node-cron';
+import { sendPushNotificationWithData } from '@/utils/firebase.helper';
 
 export async function POST(req: NextRequest) {
   await dbConnect();
 
   console.log("🔥 CRON CHAL GAYA");
 
-  return NextResponse.json({ success: true }); 
+  await sendPushNotificationWithData({
+    token: `f0dMsw4nTtyZ8ZbRCYY3FC:APA91bHnKR2YYcDnkZPyNC9oVO0Xpi57dCIhWR2DDjYEdmgpBgk2weQ6vp1yjvXiFpqQV4jg7B7pHmRsENhP5mwkUZvFn6eFpcK4b2ZcZ6NpD0SA4M7K-kk`,
+    title: 'Pharmato',
+    body: `🔥 CRON CHAL GAYA`,
+    data: {}
+  });
+
+  return NextResponse.json({ success: true });
 }
