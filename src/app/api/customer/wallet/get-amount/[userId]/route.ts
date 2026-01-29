ef8669286dd8f04cfa37958571e415a49ce695a1
work.technotoil <work.technotoil@gmail.com>
2026-01-29 17:27:57 +0530

    order 4

--- src/app/api/customer/wallet/get-amount/[userId]/route.ts
+++ src/app/api/customer/wallet/get-amount/[userId]/route.ts
@@ -49,18 +49,41 @@ export async function GET(request: NextRequest, context: { params: Promise<{ use
         return NextResponse.json({ success: false, message: 'User not found' }, { status: 404 });
     }
 
-    const checkWallet : any = await Wallet.findOne().sort({ createdAt: -1 });
-    const createdTime : any = new Date(checkWallet.createdAt);
-    const currentTime : any = new Date();
+    const checkWallet: any = await Wallet.findOne().sort({ createdAt: -1 });
+    const createdTime: any = new Date(checkWallet.createdAt);
+    const currentTime: any = new Date();
     const diffMs = currentTime - createdTime; // difference in milliseconds
     const diffMinutes = Math.floor(diffMs / (1000 * 60));
+
+    let formattedTime = 'a min';
+
+    if (diffMinutes < 60) {
+        // Only minutes
+        formattedTime = `${diffMinutes} min`;
+
+    } else if (diffMinutes < 1440) { // 24 * 60 = 1440
+        // Hours + Minutes
+        const hours = Math.floor(diffMinutes / 60);
+        const minutes = diffMinutes % 60;
+        formattedTime = `${hours} hour ${minutes} min`;
+
+    } else {
+        // Days + Hours + Minutes
+        const days = Math.floor(diffMinutes / 1440);
+        const remainingMinutes = diffMinutes % 1440;
+        const hours = Math.floor(remainingMinutes / 60);
+        const minutes = remainingMinutes % 60;
+
+        formattedTime = `${days} day ${hours} hour ${minutes} min`;
+    }
+
 
     return NextResponse.json({
         success: true,
         data: {
             _id: user._id,
             walletAmount: parseFloat(Number(user.walletAmount || 0).toFixed(2)),
-            lastUpdate : `Last update ${diffMinutes ? diffMinutes : 'a'} min ago`
+            lastUpdate: `Last update ${formattedTime} ago`
         },
         message: 'Wallet amount fetched successfully'
     });
