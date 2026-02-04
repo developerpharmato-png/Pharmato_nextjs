b4c06705b750cfefd9d8cc6fc44de0d7b8f00e2b
work.technotoil <work.technotoil@gmail.com>
2026-02-04 15:22:32 +0530

    order 3

--- src/lib/allowedOrigins.ts
+++ src/lib/allowedOrigins.ts
@@ -3,6 +3,7 @@ export const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'https://pharmatonextjs.vercel.app',
+    'https://dev.api.pharmato.bareclearact.com',
     'https://pharmato-842d3.web.app',
     'https://pharmato-nextjs.vercel.app/',
     'https://admin.technotoil.com:5175', // Added for React app CORS
