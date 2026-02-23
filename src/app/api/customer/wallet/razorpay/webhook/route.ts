537ed11abe36ba86327e829a4268cf9c92725740
work.technotoil <work.technotoil@gmail.com>
2026-02-23 18:10:37 +0530

    order 4

--- src/app/api/customer/wallet/razorpay/webhook/route.ts
+++ src/app/api/customer/wallet/razorpay/webhook/route.ts
@@ -50,18 +50,6 @@ async function runBackground(body: any) {
                     }
                 );
 
-                // Update paymentStatus in Firebase Realtime Database
-                if (checkWallet?.recharge_id) {
-                    const db = getDb();
-                    //Firebase realtime data update
-                    const firebaseRef = db.ref(`wallet/recharge/${checkWallet.recharge_id}`);
-                    const snapshot = await firebaseRef.once('value');
-                    const isRechargeStatusChanged: any = Number(snapshot.val()?.isRechargeStatusChanged || 0) + 1
-                    await firebaseRef.update({
-                        isRechargeStatusChanged: isRechargeStatusChanged
-                    });
-                }
-
                 try {
                     const captureResponse = await razorpayInstance.payments.capture(entity.id, amount, currency);
                 } catch (error) { }
